bc368a186bc66d34a6572d626a72f89f6c724bb1
Dennis <driverajr42@gmail>
2021-01-23 11:16:55 -0500

    added firebase config file in src.

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYmGnjU-VFOPTB6iWdMeGy37MsFeZPGOU",
+    authDomain: "fb-clone-f3286.firebaseapp.com",
+    projectId: "fb-clone-f3286",
+    storageBucket: "fb-clone-f3286.appspot.com",
+    messagingSenderId: "262942788793",
+    appId: "1:262942788793:web:e6236eb27829b664eedf68"
+};
