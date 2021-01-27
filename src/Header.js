aae5bfd8ccd17d0aaf6fe4674b40cb35d5c8217b
Dennis <driverajr42@gmail>
2021-01-27 12:05:34 -0500

    introduced material-ui icons in Header file.

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,41 @@
 import React from "react"
 import './Header.css';
 
+// Icons
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+
 function Header() {
     return (
         <div className="header">
             <div className="header__left">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1024px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook Logo"/>
+                <div className="header__input">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
             </div>
-            <div className="header__middle"></div>
+            <div className="header__middle">
+                <div className="header__option">
+                    <HomeIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SubscriptionsIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <StorefrontIcon fontSize="large" />
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large" />
+                </div>
+            </div>
             <div className="header__right"></div>
         </div>
     );
