166d043b823be047cdb2990beb5b3cf1ce003d3a
Dennis Rivera <dennisrivera418@gmail.com>
2021-02-01 17:58:14 -0500

    build out Login and completed styling

--- src/App.js
+++ src/App.js
@@ -4,23 +4,30 @@ import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Feed from './components/Feed/Feed';
 import Widgets from './components/Widgets/Widgets'
+import Login from './components/Login/Login'
 
 
 function App() {
+  const user = null
   return (
     <div className="app">
-      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+            <Header />
 
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
+<div className="app__body">
+  <Sidebar />
+  <Feed />
+  <Widgets />
 
 
 
-      </div>
-
-
+</div>
+        </>
+      )
+    }
     </div>
   );
 }
