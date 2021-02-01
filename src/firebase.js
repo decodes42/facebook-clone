76575e0f4261e7b3f96a704f4f929ed662a8884a
Dennis Rivera <dennisrivera418@gmail.com>
2021-02-01 18:11:21 -0500

    created a firebase db and ability for user to sign in via google

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase'
 const firebaseConfig = {
     apiKey: "AIzaSyAYmGnjU-VFOPTB6iWdMeGy37MsFeZPGOU",
     authDomain: "fb-clone-f3286.firebaseapp.com",
@@ -6,3 +7,13 @@ const firebaseConfig = {
     messagingSenderId: "262942788793",
     appId: "1:262942788793:web:e6236eb27829b664eedf68"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+// This will tell the provider we want the google sign in
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { auth, provider }
+export default db
+
