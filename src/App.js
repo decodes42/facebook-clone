6b59141d232721a12d4fd21be834e706a0b660aa
Dennis Rivera <dennisrivera418@gmail.com>
2021-02-03 12:56:56 -0500

    cleaned up code in App.js. Removed index.css file

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-
 import './App.css';
 
 import Header from './components/Dashboard/Header/Header';
 import Sidebar from './components/Dashboard/Sidebar/Sidebar';
 import Feed from './components/Dashboard/Feed/Feed';
-import Widgets from './components/Dashboard/Widgets/Widgets'
-import Login from './components/Login/Login'
-import { useStateValue } from './StateProvider'
-
+import Widgets from './components/Dashboard/Widgets/Widgets';
+import Login from './components/Login/Login';
+import { useStateValue } from './StateProvider';
 
 function App() {
-  const [{user}, dispatch] = useStateValue()
+  const [{ user }, dispatch] = useStateValue();
   return (
-    <div className="app">
+    <div className='app'>
       {!user ? (
         <Login />
       ) : (
         <>
-            <Header />
+          <Header />
 
-<div className="app__body">
-  <Sidebar />
-  <Feed />
-  <Widgets />
-
-
-
-</div>
+          <div className='app__body'>
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
         </>
-      )
-    }
+      )}
     </div>
   );
 }
