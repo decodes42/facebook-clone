6947f18815a7c3a26bc197ac49bc21fdd41e1e2c
Dennis Rivera <dennisrivera418@gmail.com>
2021-02-09 15:15:04 -0500

    added logo and h1 title for site on Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Login.css'
+import logo from '../../logo.svg'
+import { Typography } from '@material-ui/core';
 import { auth, provider } from '../../firebase'
 import { Button } from '@material-ui/core'
 import { useStateValue } from '../../StateProvider'
@@ -26,9 +28,9 @@ function Login() {
     return (
         <div className='login'>
             <div className="login__logo">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1024px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook Logo"/>
+                <img src={logo} alt="Dev Space Logo"/>
+                <Typography className ='login__title' variant='h1'>{'devSpace'}</Typography>
 
-                <img src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg" alt=""/>
             </div>
 
             {/* <Button type='submit' onClick={signIn}>
